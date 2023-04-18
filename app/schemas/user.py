2849f104c5fe475835c9ad42c267933a796afce0
xudoyberdi <xudoyberdi.odilov@yandex.ru>
2023-04-18 23:13:49 +0300

    commit 5

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,15 +1,1 @@
-from fastapi_users import models
-
-
-class UserCreate(models.BaseUserCreate):
-    '''Cхема для регистрации пользователя; в неё обязательно
-    должны быть переданы email и password.
-    '''
-    pass
-
-
-class UserUpdate(models.BaseUserUpdate):
-    '''Cхема для обновления объекта пользователя; содержит все базовые поля модели
-    (в том числе и пароль). Все поля опциональны.
-    '''
-    pass
+#
