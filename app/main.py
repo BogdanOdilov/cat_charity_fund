a25d61b168e73323be66d8b43d461ea7a0a55d98
xudoyberdi <xudoyberdi.odilov@yandex.ru>
2023-04-19 21:37:39 +0300

    update api

--- app/main.py
+++ app/main.py
@@ -1,14 +1,22 @@
+import uvicorn
 from fastapi import FastAPI
 
-from api.routers import main_router
-from core.config import settings
-from core.init_db import create_first_superuser
+# Импортируем роутер.
+from app.api.routers import main_router
+from app.core.config import settings
+# Импортируем корутину для создания первого суперюзера.
+from app.core.init_db import create_first_superuser
 
 app = FastAPI(title=settings.app_title)
 
+# Подключаем роутер.
 app.include_router(main_router)
 
-
+# При старте приложения запускаем корутину create_first_superuser.
 @app.on_event('startup')
 async def startup():
     await create_first_superuser()
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, debug=True)
